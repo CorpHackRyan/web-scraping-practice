e47cd7e3db5144f635551f9d7674fd5be049332a
krete77 <ryanoconnor.dev@gmail.com>
2022-06-08 13:14:29 -0400

    initial commit

---
+++ web_scraping.py
@@ -0,0 +1,11 @@
+# Ryan OConnor - 6/8/22
+
+# Web Scraping
+# Working with urlLib
+# Filtering data using reg ex
+# Web scraping using beautiful soup
+
+
+# Data sources from below web sites
+# https://www.regelleistung.net/ext/data/
+# https://agsi.gie.eu/api?date=2022-04-06
