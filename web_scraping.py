f185e844a7fd0b44ccc6a35314ec92ff42f12d59
krete77 <ryanoconnor.dev@gmail.com>
2022-06-08 13:43:28 -0400

    added readme.MD, updated py file

--- web_scraping.py
+++ web_scraping.py
@@ -1,11 +1,34 @@
-# Ryan OConnor - 6/8/22
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
 
-# Web Scraping
-# Working with urlLib
-# Filtering data using reg ex
-# Web scraping using beautiful soup
+url = 'https://www.iplt20.com/stats/2022'
+req = requests.get(url)
 
+# 200 means we can successfully scrape the data
+print(req.status_code)
 
-# Data sources from below web sites
-# https://www.regelleistung.net/ext/data/
-# https://agsi.gie.eu/api?date=2022-04-06
+# print the entire html content
+# data is of type 'bytes', NOT string
+print(req.content)
+
+# export to html file with 'wb' as 'write bytes'
+with open('ipl20.html', 'wb') as fobj:
+    fobj.write(req.content)
+
+# store the page into a variable
+data = req.content
+print(data)
+
+# let's use beautiful soup to hash the data
+# goto inspect element in browser, click on network, paste the url, and you get this information
+# fetching without the table data
+req = requests.get(url)
+print(req.headers, req.cookies)
+
+# fetching with table data
+headers_info = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0'}
+req = requests.get(url, headers=headers_info)
+print(req.status_code)
+
